141a69f9edc0234ce68223a0775f3c49f9c4adb0
Derreumaux <valentinderreumaux03@gmail.com>
2020-05-11 11:31:34 +0200

    Update insertsite.php

--- Projet1.0/forms/insertsite.php
+++ Projet1.0/forms/insertsite.php
@@ -7,20 +7,20 @@ $categories = $result->fetchAll();
 ?>
 <main class="container">
     <section id="insert">
-        <h2 class="display-4 my-3">Insertion d'un site</h2>
+        <h2 class="display-4 my-3">Insertion d'un restaurant</h2>
         <div class="row">
             <div class="col-md-12">
                 <form action="src/insertsite.php" method="post">
                     <div class="form-group">
-                        <label for="name">Nom du site</label>
+                        <label for="name">Nom du restaurant</label>
                         <input type="text" id="name" name="name" placeholder="Nom du site" required class="form-control">
                     </div>
                     <div class="form-group">
-                        <label for="url">URL du site</label>
+                        <label for="url">URL du restaurant</label>
                         <input type="url" id="url" name="url" placeholder="URL du site" required class="form-control">
                     </div>
                     <div class="form-group">
-                        <label for="category">Catégorie du site</label>
+                        <label for="category">Catégorie du restaurant</label>
                         <select id="category" name="category" required class="form-control">
                             <option value="" disabled selected>Sélectionnez...</option>
                             <?php foreach ($categories as $category) : ?>
@@ -29,7 +29,7 @@ $categories = $result->fetchAll();
                         </select>
                     </div>
 
-                    <label for="note">Evaluation du site</label>
+                    <label for="note">Evaluation du restaurant</label>
                     <select id="note" name="note" required class="form-control">
                         <option value="" disabled selected>Sélectionnez...</option>
                         <?php for($i = 1; $i <= 5; $i++) : ?>
@@ -43,7 +43,7 @@ $categories = $result->fetchAll();
                         <!--                        <option value="5">⭐⭐⭐⭐⭐</option>-->
                     </select>
                     <div class="form-group">
-                        <label for="description">Description du site</label>
+                        <label for="description">Description du restaurant</label>
                         <textarea name="description" id="description" rows="4" class="form-control" required></textarea>
                     </div>
                     <input type="submit" class="btn btn-primary">
