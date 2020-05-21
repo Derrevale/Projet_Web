cfa1e59e223bb3d25993917bc8736f9b9e4c7ea7
HE201559 <56299123+HE201559@users.noreply.github.com>
2020-05-21 18:26:00 +0200

    Update index.php

--- Projet2/index.php
+++ Projet2/index.php
@@ -23,43 +23,48 @@ else {
 $result->execute();
 $websites = $result->fetchAll()
 ?>
-<main class="container">
-    <section id="listing">
-        <h3 class="display-4 my-3">Liste des Restaurants</h3>
-        <div class="row">
-            <div class="col-md-12">
-                <?php if (count($websites)) : ?>
+    <link href="css/ahover.css" rel="stylesheet">
 
-                    <table class="table table-hover">
-                        <thead>
-                        <tr>
-                            <th>Nom</th>
-                            <th>Url</th>
-                            <th>Catégorie</th>
-                            <th>Note</th>
-                            <th>Commentaire</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <?php foreach ($websites as $website) : ?>
-                            <tr>
-                                <td><?= $website->name ?></td>
-                                <td><a href="<?= $website->url ?>">Voir le site</a></td>
-                                <td><?= $website->category ?></td>
-                                <td><?= stars($website->note) ?></td>
-                                <td><a href="avis.php?search=<?= $website->name ?>">avis d'utilisateur</a></td>
-                            </tr>
-                        <?php endforeach; ?>
-                        </tbody>
-                    </table>
-                <?php else: ?>
-                    <h3>Aucun enregistrement pour <?= $_GET['search'] ?></h3>
-                <?php endif; ?>
+    <main class="container">
+        <section id="listing">
+            <h3 class="display-4 my-3">Liste des Restaurants</h3>
+
+            <div class="row">
+
+                <?php foreach ($websites as $website) : ?>
+
+                    <div class="col-12 col-lg-4">
+                        <div class="card" style="background-image: url('image/<?= $website->name ?>.jpg')">
+
+                            <h1><?= $website->name ?></h1>
+                            <h3><?= $website->category ?></h3><br>
+                            <h4><?= stars($website->note) ?></h4>
+                            <h5><a href="avis.php?search=<?= $website->name ?>">avis d'utilisateur</a></h5>
+                            <h5><a href="<?= $website->url ?>">Voir le site</a></h5><br><br>
+                        </div>
+                    </div>
+
+
+                <?php endforeach; ?>
             </div>
-        </div>
-    </section>
-</main>
+            <div class="bg-light">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <ul class="list-inline text-center">
+                                <li class="list-inline-item"><a href="#">À propos</a></li>
+                                <li class="list-inline-item">&middot;</li>
+                                <li class="list-inline-item"><a href="#">Vie privée</a></li>
+                                <li class="list-inline-item">&middot;</li>
+                                <li class="list-inline-item"><a href="#">Conditions d'utilisations</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    </main>
 
 <?php
-require 'partials/footer.php';
-?>
+require 'partials/footer.php'; ?>
