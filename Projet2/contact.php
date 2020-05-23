37e1ffc7d4454361e20906a4e3e4013406fe6fb1
HE201559 <56299123+HE201559@users.noreply.github.com>
2020-05-23 17:22:22 +0200

    Update contact.php

--- Projet2/contact.php
+++ Projet2/contact.php
@@ -1,61 +1,64 @@
 <?php
+//Page principale , acces utilisateur normaux si connection effective
 require 'partials/header.php';
 require 'src/DB.php';
 require 'src/tools.php';
+$dbh = connect();
+if (!isset($_GET['search'])) {
+    $sql = "SELECT s.id, s.name,s.url, s.note, c.category
+    FROM sites s 
+    LEFT JOIN category c on s.cat_id = c.id
+    order by name";
+    $result = $dbh->prepare($sql);
+}
+else {
+    $sql = "SELECT s.id, s.name,s.url, s.note, c.category
+    FROM sites s 
+    LEFT JOIN category c on s.cat_id = c.id
+    WHERE s.name LIKE :search
+    order by name";
+    $result = $dbh->prepare($sql);
+    $result->bindValue('search', '%'.$_GET['search']. '%', PDO::PARAM_STR);
+}
+$result->execute();
+$websites = $result->fetchAll()
 ?>
- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
- <link href="css/cssglobal.css" rel="stylesheet">
+    <link href="css/cssglobal.css" rel="stylesheet">
 
-<main class="container">
+    <main class="container">
         <div class="row">
-            <div class="col">
-                 <div class="jumbotron">
-                     <h1>Contact</h1>
-                 </div>
+            <div class="col-12 col-lg-4">
+                <h1 id="nom">Resto-rank</h1>
             </div>
         </div>
-    <div class="row">
-        <div class="col">
-            <div class="text-center">
-                 <img src="image/bo.jpg" alt="b">
-            </div>
-        </div>
-    </div>
-    <br><br>
-    <div class="row">
-        <div class="col-12 col-lg-4">
-            <div class="card">
-                <img class="card-img-top" src="image/mail.jpg" alt="mail" >
-                <div class="card-body">
-                    <h4 class="card-title"> Email </h4>
-                    <p class="card-text"> Contactez nous via : resto-rank@gmail.com</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-12 col-lg-4">
-            <div class="card">
-                <img class="card-img-top" src="image/tel.jpg" alt="tel" >
-                <div class="card-body">
-                    <h4 class="card-title"> Mobile </h4>
-                    <p class="card-text"> Contactez nous via ce numéro : 0135433</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-12 col-lg-4">
-            <div class="card">
-                <img class="card-img-top" src="image/maison.jpg" alt="maison">
-                <div class="card-body">
-                    <h4 class="card-title"> Adresse </h4>
-                    <p class="card-text"> Retrouvez nous "Rue du parpaing n°15</p>
-                </div>
-            </div>
-        </div>
-    </div>
+        <section id="listing">
+            <h3 class="display-4 my-3">Liste des Restaurants</h3>
 
-</main>
+            <div class="row">
+
+                <?php foreach ($websites as $website) : ?>
+
+                    <div class="col-12 col-lg-4">
+                        <div class="card" style="background-image: url('image/<?= $website->name ?>.jpg')">
+
+                            <h1><?= $website->name ?></h1>
+                            <h3><?= $website->category ?></h3><br>
+                            <h4><?= stars($website->note) ?></h4>
+                            <h5><a href="avis.php?search=<?= $website->name ?>">avis d'utilisateur</a></h5>
+                            <h5><a href="<?= $website->url ?>">Voir le site</a></h5>
+                        </div>
+                    </div>
 
 
+
+                <?php endforeach; ?>
+            </div>
+
+        </section>
+    </main>
 <?php
 require 'partials/footer.php';
 ?>
+
+
 
