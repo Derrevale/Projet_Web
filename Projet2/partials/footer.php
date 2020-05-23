d04169c5c15ef72798790b7c2b6a44f570db147f
HE201559 <56299123+HE201559@users.noreply.github.com>
2020-05-23 17:30:15 +0200

    Update footer.php

--- Projet2/partials/footer.php
+++ Projet2/partials/footer.php
@@ -2,5 +2,23 @@
 
 <script src="js/jquery.min.js"></script>
 <script src="js/bootstrap.bundle.min.js"></script>
-</body>
+<html>
+<footer>
+    <div class="bg-light">
+        <div class="container">
+            <div class="row">
+                <div id="footer" class="col">
+
+                    <ul class="list-inline text-center">
+                        <li class="list-inline-item"><a href="#">À propos</a></li>
+                        <li class="list-inline-item">&middot;</li>
+                        <li class="list-inline-item"><a href="#">Vie privée</a></li>
+                        <li class="list-inline-item">&middot;</li>
+                        <li class="list-inline-item"><a href="#">Conditions d'utilisations</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 </html>
