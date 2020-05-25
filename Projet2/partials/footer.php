0b87c651b714ed0b00fc3cc7707b52fe8b618d6e
HE201559 <56299123+HE201559@users.noreply.github.com>
2020-05-25 12:37:06 +0200

    Update footer.php

--- Projet2/partials/footer.php
+++ Projet2/partials/footer.php
@@ -10,11 +10,11 @@
                 <div id="footer" class="col">
 
                     <ul class="list-inline text-center">
-                        <li class="list-inline-item"><a href="#">À propos</a></li>
+                        <li class="list-inline-item"><a href="propos.php">À propos</a></li>
                         <li class="list-inline-item">&middot;</li>
-                        <li class="list-inline-item"><a href="#">Vie privée</a></li>
+                        <li class="list-inline-item"><a href="prive.php">Vie privée</a></li>
                         <li class="list-inline-item">&middot;</li>
-                        <li class="list-inline-item"><a href="#">Conditions d'utilisations</a></li>
+                        <li class="list-inline-item"><a href="conditions.php">Conditions d'utilisations</a></li>
                     </ul>
                 </div>
             </div>
