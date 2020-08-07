3554591b272f80b5eb446a4057d655f4812ebd58
HE201559 <56299123+HE201559@users.noreply.github.com>
2020-08-08 01:10:34 +0200

    Update propos.php

--- Projet2/propos.php
+++ Projet2/propos.php
@@ -6,10 +6,15 @@ require 'src/tools.php';
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <link href="css/cssglobal.css" rel="stylesheet">
+<script type="text/javascript" src="js/fonctions.js"></script>
 
 <head>
     <title>A propos</title>
 </head>
+<div>
+    <button  id="sombre"  onclick="modeNuit('dimgray')"> Mode Nuit </button>
+    <button id="jour" onclick="modeJour('blanchedalmond')"> Mode Jour </button>
+</div>
 <body>
     <div id="prop">
         <h3 class="titregras"> A propos </h3>
