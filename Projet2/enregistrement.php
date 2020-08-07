e93c5436e6ee37c6c86ac788c6337e80ee501ccc
HE201559 <56299123+HE201559@users.noreply.github.com>
2020-08-08 01:08:43 +0200

    Update enregistrement.php

--- Projet2/enregistrement.php
+++ Projet2/enregistrement.php
@@ -1,10 +1,14 @@
+<div>
+    <button  id="sombre"  onclick="modeNuit('dimgray')"> Mode Nuit </button>
+    <button id="jour" onclick="modeJour('blanchedalmond')"> Mode Jour </button>
+</div>
 <?php
 /*possibiliter de modifier la db directement depuis le site en tant que admin
 (objectif lors de la connection si admin direction vers celui si et non le user classique
 */
 require 'partials/header_admin.php';
-require 'tools/DB.php';
-require 'tools/tools.php';
+require 'src/DB.php';
+require 'src/tools.php';
 $dbh = connect();
 if (isset($_GET['action'])){
     if ($_GET['action'] === 'enregistrement') {
@@ -18,3 +22,7 @@ if (isset($_GET['action'])){
 
 require 'partials/footer.php';
 ?>
+
+<script type="text/javascript" src="js/fonctions.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link href="css/cssglobal.css" rel="stylesheet">
