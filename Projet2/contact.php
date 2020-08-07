8454c316f8c97374cdea16b770f5ac5faf76208a
HE201559 <56299123+HE201559@users.noreply.github.com>
2020-08-08 01:20:47 +0200

    Update contact.php

--- Projet2/contact.php
+++ Projet2/contact.php
@@ -6,10 +6,6 @@ require 'src/tools.php';
  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
  <link href="css/cssglobal.css" rel="stylesheet">
 <script type="text/javascript" src="js/fonctions.js"></script>
-<div>
-    <button  id="sombre"  onclick="modeNuit('dimgray')"> Mode Nuit </button>
-    <button id="jour" onclick="modeJour('blanchedalmond')"> Mode Jour </button>
-</div>
 <body>
 <main class="container">
         <div class="row">
