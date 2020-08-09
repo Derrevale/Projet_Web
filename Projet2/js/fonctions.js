ccd46d02cbfede76dd4a7acf8214e2c55502ce81
HE201559 <56299123+HE201559@users.noreply.github.com>
2020-08-09 20:14:04 +0200

    Update fonctions.js

--- Projet2/js/fonctions.js
+++ Projet2/js/fonctions.js
@@ -1,27 +1,21 @@
-let x = document.getElementsByClassName('type');
-let t =  document.getElementsByClassName('resto');
+let ima =document.getElementsByClassName('card');
+
 
 function modeNuit(color) {
 
     document.body.style.background = color;
-
-    for(let i in x){
-        x[i].style.color="#0B5345";
+    for(let i in ima){
+        ima[i].style.filter='brightness(0.7)';
     }
 
-    for(let i in t){
-        t[i].style.color="#0B5345";
-    }
+
 
 }
 
 function modeJour(color){
     document.body.style.background = color;
-    for(let i in x){
-        x[i].style.color='#EC7063';
+    for(let i in ima){
+        ima[i].style.filter='brightness(1)';
     }
 
-    for(let i in t){
-        t[i].style.color='#EC7063';
-    }
 }
