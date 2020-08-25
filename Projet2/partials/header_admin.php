370b71f3d7f0cd0fe733e4f679e7acda327b3b72
HE201559 <56299123+HE201559@users.noreply.github.com>
2020-08-25 22:03:58 +0200

    Update header_admin.php

--- Projet2/partials/header_admin.php
+++ Projet2/partials/header_admin.php
@@ -1,4 +1,23 @@
 <!-- Derreumaux Valentin -  20/04/2020  13:27-->
+<?php
+// On prolonge la session
+session_start();
+// On teste si la variable de session existe et contient une valeur
+if(empty($_SESSION['login']))
+{
+// Si inexistante ou nulle, on redirige vers le formulaire de login
+header('Location:../web/index.php');
+exit();
+}
+
+if(empty($_SESSION['typeofuser']))
+{
+// Si inexistante ou nulle, on redirige vers le formulaire de login
+    header('Location:../web/index.php');
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
