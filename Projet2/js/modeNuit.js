901de9ec21eb90cf57ad80f1af9e92ec27f6c670
HE201559 <56299123+HE201559@users.noreply.github.com>
2020-08-25 21:21:45 +0200

    Update modeNuit.js

--- Projet2/js/modeNuit.js
+++ Projet2/js/modeNuit.js
@@ -1,26 +1,15 @@
-var imaindex =document.getElementsByClassName('card');
-var nav=document.getElementById("navTop");
-
+let ima =document.getElementsByClassName('card');
 
 
 function modeNuit(color) {
-    var basP = document.getElementById("pied");
-    nav.style.filter= 'brightness(0.7)';
-    basP.style.filter= 'brightness(0.7)';
-    document.body.style.background = color;
-    for (let i in imaindex) {
-        imaindex[i].style.filter = 'brightness(0.7)';
-    }
+    window.sessionStorage.mode="nuit";
+    document.location.reload(true);
+
+
 
 }
 
 function modeJour(color){
-    var basP = document.getElementById("pied");
-    nav.style.filter= 'brightness(1)';
-    basP.style.filter= 'brightness(1)';
-    document.body.style.background = color;
-    for(let i in imaindex){
-        imaindex[i].style.filter='brightness(1)';
-    }
-
+    window.sessionStorage.mode="jour";
+    document.location.reload(true);
 }
