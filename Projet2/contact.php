c8363c0912bea652cd36f7632703815992c1adb2
HE201559 <56299123+HE201559@users.noreply.github.com>
2020-08-27 15:00:35 +0200

    Update contact.php

--- Projet2/contact.php
+++ Projet2/contact.php
@@ -15,10 +15,21 @@ require 'tools/tools.php';
                  </div>
             </div>
         </div>
-    <div class="row mx-auto d-block text-center">
-        <div class>
-            <div>
-                 <img class="img-fluid" src="image/bo.jpg" alt="b">
+    <div class="row mx-auto d-block  text-center">
+        <div class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="image/carou3.jpg">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="image/bo.jpg">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="image/carou4.jpg">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="image/carou5.jpg">
+                </div>
             </div>
         </div>
     </div>
